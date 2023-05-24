5c4b8b25a18bab516db40097d4849d97464e7127
Sindhu Vaddi <102273962+SindhVaddi@users.noreply.github.com>
2023-05-24 01:50:06 -0400

    deploying app

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Layout from "../components/Layout/Layout";
+import Layout from "../components/Layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
